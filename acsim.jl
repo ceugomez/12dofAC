c9fac415a7908a6d62c84123b66614e8eb2d06dd
ceu <cego6160@colorado.edu>
2024-11-20 10:18:20 -0700

    julia pkg management manifest

--- acsim.jl
+++ acsim.jl
@@ -5,7 +5,6 @@
 struct Parameters
     # Define any necessary fields for aircraft parameters here
 end
-
 # Define aircraft state structure
 struct AcState
     pe::Vector{Vector{Float64}}    # Position in Earth coordinates
@@ -13,7 +12,6 @@ struct AcState
     vbe::Vector{Vector{Float64}}   # Velocity in the body frame
     veang::Vector{Vector{Float64}} # Angular velocity in the body frame
 end
-
 # State derivatives as a function of state, wind, control, and aircraft parameters
 function ACstatederiv(state::AcState, w, u, param::Parameters)
     # state: [N E D u v w phi theta psi p q r]
@@ -27,36 +25,12 @@ function ACstatederiv(state::AcState, w, u, param::Parameters)
     
     return statedot
 end
-
 # Function to compute the rotation matrix from Euler angles
 function Rbe(eang::Vector{Float64})
     Rbe = [cos(eang[2])*cos(eang[1]) -sin(eang[1]) 0;  #Not done
            sin(eang[2])*cos(eang[1]) cos(eang[1])  0;
            0                      0                1]
     return Rbe
-end
-
-# State derivative of a balloon in a wind field (to be implemented)
-function balloonstatederiv(state)
-    # Placeholder for balloon state derivative computation
-    return state
-end
-
-# Returns inertial wind vector as a function of inertial position (E, N, U)
-function windField(pos)
-    W_N = 10  # North wind component
-    W_E = 0   # East wind component
-    W_U = 1   # Updraft component (f(x, y, z) if position-dependent)
-    return [W_N, W_E, W_U]
-end
-
-# Runge-Kutta 4th-order integrator
-function RK4_int(dt, state, fdot)
-    k1 = dt * fdot(state)
-    k2 = dt * fdot(state + 0.5 * k1)
-    k3 = dt * fdot(state + 0.5 * k2)
-    k4 = dt * fdot(state + k3)
-    return state + (k1 + 2 * k2 + 2 * k3 + k4) / 6.0
 end
 begin
     println("Starting Simulation")
