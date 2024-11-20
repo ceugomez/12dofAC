77abd0cc88570c783a8c3bad326b191b5c8e4ed6
ceu <cego6160@colorado.edu>
2024-11-20 09:37:23 -0700

    restructure

---
+++ balloonsim.jl
@@ -0,0 +1,5 @@
+# State derivative of a balloon in a wind field (to be implemented)
+function balloonstatederiv(state)
+    # Placeholder for balloon state derivative computation
+    return state
+end
