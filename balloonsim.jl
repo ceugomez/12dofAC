0feffb1d537c3de7e24747e28e9eb25e0a1a2008
ceu <cego6160@colorado.edu>
2024-11-27 10:38:08 -0500

    std atm dependency, state int, etc

--- balloonsim.jl
+++ balloonsim.jl
@@ -2,39 +2,99 @@ include("generic.jl");
 using Parameters
 # State derivative of a balloon in a wind field (to be implemented)
 @with_kw mutable struct balloonParams
+    # known from swenson et al; 
+    # payload mass: 91g 
+    # 125 L volume, ~ 0.125 m^3
     gravity::Float64 = 9.81
     ρa::Float64 = 1.225 # kg/m^3        !! Modifiable based on state
     ρh::Float64 = 0.166 # kg/m^3, helium
     Cd::Float64 = 0.01  # drag coeff    !!Placeholder
     r::Float64 = 1;     # m,            !!Placeholder
-    vol::Float64 = 2;   # m^3,          !!Placeholder
-    m::Float64 = 0.1    # kg            !!Placeholder
+    vol::Float64 = 0.125;   # m^3,         
+    m::Float64 = 0.92    # kg       
 end
-@with_kw mutable struct balloonState
-    pos::Vector{Float64} = [0.0,0.0,0.0]    # inertial position, m
-    vel::Vector{Float64} = [0.0,0.0,0.0]    # inertial velocity, m/s
-end
-mutable struct balloonStateDeriv
-    vel::Vector{Float64}                    # inertial velocity, m/s
-    acc::Vector{Float64}                    # inertial acceleration m/s^2
-end
-function balloonstatederiv(x::balloonState, prm::balloonParams)
+function balloonstatederiv(x::Vector{Float64})
     # buoyancy force 
-    Fb = (prm.ρa-prm.ρh)*g*prm.vol                  # 
+    fb = (prm.ρa-prm.ρh)*prm.gravity*prm.vol                  
+    Fb = Vector{Float64}([0.0,0.0,fb])                                      # only in z dir
     # drag force
-    Vrel = x.vel - windField(x.pos); 
-    Fd = prm.Cd * 0.5 * prm.ρa * Vrel.^2 * prm.R;   # F = 1/2 * ρa * Vrel^2  * S * Cd
-    a = (Fb+F)/m;                                   # a = ∑F/m
-    xdot = balloonStateDeriv(x.vel,a);
+    Vrel = x[4:6] - windField(x[1:3]); 
+    Fd = prm.Cd * 0.5 * prm.ρa * norm(Vrel.^2) * prm.r;                     # F = 1/2 * ρa * Vrel^2  * S * Cd
+    Fd = -normalize(Vrel)*Fd;                                               # opposes relative motion
+    # a = ∑F/m
+    a = (Fb+Fd)./prm.m;                                
+    # return state derivative
+    xdot = [x[4:6]; a]; #vcat
     return xdot
 end
+function getDensity(z)
+
+    
+end
+function VisualizeFieldXYZ(history::Matrix{Float64})
+    plt = figure();
+    title("Balloon Ground Track");
+    # NE track
+        ax1 = plt.add_subplot(131);
+        ax1.plot(history[1,:], history[2,:]);
+        xlabel("Easting [m]")
+        ylabel("Northing [m]")
+        # EU track
+        ax2 = plt.add_subplot(132);
+        ax2.plot(history[2,:], history[3,:]);
+        xlabel("Easting [m]")
+        ylabel("Up [m]")
+        # 3d plot
+        ax3 = plt.add_subplot(133);
+        ax3.plot(history[1,:],history[3,:]);
+        xlabel("Northing[m]");
+        ylabel("Up [m]")
+    savefig("./figures/Track")
+    show()
+    return nothing
+end
+function VisualizeFieldT(history::Matrix{Float64})
+    plt = figure();
+    title("Balloon Time History");
+    t = LinRange(0, 3600/60, 3601)
+
+    # NE track
+        ax1 = plt.add_subplot(131);
+        ax1.plot(t, history[1,:]);
+        xlabel("Time [s]")
+        ylabel("Easting [m]")
+        # EU track
+        ax2 = plt.add_subplot(132);
+        ax2.plot(t, history[2,:]);
+        xlabel("Time [s]")
+        ylabel("Northing [m]")
+        # 3d plot
+        ax3 = plt.add_subplot(133);
+        ax3.plot(t,history[3,:]);
+        xlabel("Time [s]");
+        ylabel("Up [m]")
+    savefig("./figures/TTrack")
+    show()
+    return nothing
+end
 begin
+
     println("Simulating...");
-    history = Vector{balloonState}()
-    state = balloonState();
-    for i in 1:100
+    # initialize parameter structure
+    global prm = balloonParams();
+    # initialize history vector
+    tstep = 0.1;
+    tf = 3600
+    history = zeros(Float64, 6,tf+1);
+
+    for i in 1:(tf)
         #  integrate
-        state = RK4_int(0.1, state,balloonStateDeriv);
-        push!(history, state)
+        stateK = history[:,i];
+        stateKp1 = RK4_int(tstep, stateK, balloonstatederiv)
+        history[:,i+1] = stateKp1; # integrate
     end
+    println("done")
+    #VisualizeFieldXYZ(history)
+    VisualizeFieldT(history)
+
 end
--- generic.jl
+++ generic.jl
@@ -1,14 +1,14 @@
 using LinearAlgebra, BenchmarkTools, PyPlot, Optimization
 # Returns inertial wind vector as a function of inertial position (E, N, U)
 function windField(pos)
-    W_N = 10  # North wind component
-    W_E = 0   # East wind component
-    W_U = 1   # Updraft component (f(x, y, z) if position-dependent)
-    return [W_N, W_E, W_U]
+    W_N = 10*pos[3]   # North wind component
+    W_E = 0    # East wind component
+    W_U = 25   # Updraft component (f(x, y, z) if position-dependent)
+    return [W_N; W_E; W_U]
 end
 
 # Runge-Kutta 4th-order integrator
-function RK4_int(dt, state, fdot)
+function RK4_int(dt, state, fdot::Function)
     k1 = dt * fdot(state)
     k2 = dt * fdot(state + 0.5 * k1)
     k3 = dt * fdot(state + 0.5 * k2)
